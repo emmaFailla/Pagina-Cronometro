d7b99d032edca6d1272ee23a0a95f1badfabe2b0
Emmanuel Failla <emmafailla@gmail.com>
2022-08-11 18:25:44 -0300

    Minutos y horas añadidos :D

--- app.js
+++ app.js
@@ -1,24 +1,48 @@
 
-let tiempo = 0
-let intervalo = 0
-let verificador = false
+let tiempo =58;
+let intervalo = 0;
+let hora = 0;
+let minutos = 0;
+let verificador = false;
+let arrayLimitador = [];
+
 const cronometro = document.querySelector(".cronometro")
 const alternado = document.querySelector(".startStop")
 const reset = document.querySelector(".reset")
+const grabar = document.querySelector(".grabar")
+const tiemposGrabados= document.querySelector(".tiemposGrabados")
 
 
 //Funcion de tiempo
 function inicio (){
     
     if(verificador == false){
-    intervalo = setInterval(()=>{
 
+    intervalo = setInterval(()=>{
     tiempo += 0.01 
-    cronometro.innerHTML =  tiempo.toFixed(2)
+    cronometro.innerHTML = `${hora}:${minutos}:${tiempo.toFixed(2)}` 
     verificador = true
+
+    //Conversion a minutos
+    if(tiempo.toFixed(2) == 60.00){
+        minutos+=1
+        tiempo = 0.00
+        
+        
+        
+    }
+    //Conversion a horas
+    else if(minutos == 60){
+        hora+=1
+        minutos = 0
+        tiempo = 0.00
+    }
+
+    
 
     },10)
     }
+    
 
     else {
         clearInterval(intervalo)
@@ -40,13 +64,40 @@ function reseteo(){
 
         clearInterval(intervalo)
         tiempo = 0
+        minutos = 0
+        hora = 0
         cronometro.innerHTML = "0.00"
         verificador = false
     }
+
+    while(tiemposGrabados.firstChild){
+        tiemposGrabados.removeChild(tiemposGrabados.firstChild)
+    }
+
+    
     
 }
 //Reseteo
 
+//grabacion
+function grabacion(){
+    if(cronometro.innerHTML == "0.00"){
+        alert("Empeza a correr el cronometro para grabar")
+    }
+    else{
+        let p = document.createElement('ul');
+    p.style.listStyle = "none"
+    p.innerHTML += `
+    <li>Tiempo : ${cronometro.innerHTML} </li>
+    `;
+    tiemposGrabados.appendChild(p)
+    }
+
+
+}
+//grabacion
+
 //Eventos
 alternado.addEventListener('click',inicio)
 reset.addEventListener('click',reseteo)
+grabar.addEventListener('click',grabacion)
